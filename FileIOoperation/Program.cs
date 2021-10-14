fc4aed3fad067cb607236d49c3d5e02b5cbc286d
Dipti104706 <diptimayeebehura104@gmail.com>
2021-10-14 09:06:04 +0530

    [Dipti] added code to check txt file exist or not

---
+++ FileIOoperation/Program.cs
@@ -0,0 +1,17 @@
+﻿using FileIOoperation.FileIO;
+using System;
+
+namespace FileIOoperation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to File IO Operations");
+            string path = @"H:\visualstudio\FileIOoperation\FileIOoperation\FileIO\Sample.txt";
+            FileReadWriteOperation file = new FileReadWriteOperation();
+            file.FilExists(path);
+            Console.ReadLine();
+        }
+    }
+}
